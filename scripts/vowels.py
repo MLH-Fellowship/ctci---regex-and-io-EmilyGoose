00cde48b9360f6d8fda21bbb13bacd808e5b351d
EmilyGoose <EmilyGoose@users.noreply.github.com>
2022-08-17 03:43:32 -0400

    Solve vowels

--- scripts/vowels.py
+++ scripts/vowels.py
@@ -1,1 +1,16 @@
 # Write the solution for "Filter Vowels".
+import re
+
+# Open the file
+f = open("../inputs/vowels.txt", "r")
+long_text = f.read()
+f.close()
+
+# Create regex for matching single vowel and find matches
+word_regex = re.compile("[aeiou]")
+vowels = word_regex.findall(long_text)
+
+# Append count to filename
+out_file = open("vowels-" + str(len(vowels)) + ".txt", "w")
+out_file.write(long_text)
+out_file.close()
